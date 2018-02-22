125a276fdb2c56c88ce963a9ca8de19d7f871218
a_dzesh <krug@tut.by>
2018-02-22 22:54:01 +0300

    function solveEquation was implemented

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+    
+    var str = equation.replace(/\s/g, '');
+    var a = str.split('*')[0];
+    var b = (str.split('^2')[1]).split('*x')[0]; 
+    var c = str.substring(str.lastIndexOf('x')+1);
+    var result = (-1 * b - Math.sqrt(Math.pow(b, 2) - (4 * a * c))) / (2 * a);
+    var result2 = (-1 * b + Math.sqrt(Math.pow(b, 2) - (4 * a * c))) / (2 * a);
+    
+    var arr = [];
+    
+    if (a<0) {
+        arr.push(Math.round(result2));
+        arr.push(Math.round(result));
+    }
+    else {
+        arr.push(Math.round(result));
+        arr.push(Math.round(result2));
+    }
+return arr;
 }
